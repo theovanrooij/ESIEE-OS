af03dbab5f3cf9b7ac92cc36e723f64834e037e5
Théo Van Rooij <38379878+theovanrooij@users.noreply.github.com>
2021-01-04 17:41:10 +0100

    Update exo2.c

--- TDm2/exo2.c
+++ TDm2/exo2.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[]){
     pid_t fils_pid = fork();
     if(fils_pid ==0){
-        execvp("./testIB1",NULL);// Le père du nouveau a le meme pid que le programme actuel, c'est le comportement attendu.
+        execl("./testIB1", "./testIB1", NULL);// Le père du nouveau a le meme pid que le programme actuel, c'est le comportement attendu.
     } else if (fils_pid > 0) {
         afficheCourant("Je suis le père.");// 1 processus
         wait(NULL); // Ajout wait
